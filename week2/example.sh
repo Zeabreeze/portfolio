c9fac457af309d02591cb6b1ca66a0d577987a28
Zeabreeze <ibenjaba@our.ecu.edu.au>
2023-01-21 08:26:41 +0000

    Week 2 Scripts

---
+++ week2/example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello, please enter your name"
+
+read name
+
+echo "Hello $name! Welcome to \"Scripting Languages\""
+
+exit 0
---
+++ week2/foldermaker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Type the name of the folder you would like to create ->  " folderName
+mkdir "$folderName"
