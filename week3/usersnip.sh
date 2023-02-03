ad9580e40e6e8bea2f22e3684b342e670c9b06d0
Zeabreeze <ibenjaba@our.ecu.edu.au>
2023-02-03 04:39:59 +0000

    Week 3 scripts to portfolio

--- week3/usersnip.sh
+++ week3/usersnip.sh
@@ -16,3 +16,18 @@ case var in
 esac
 
 read -p 'The prompt text goes here' var
+
+if [[ {Andarg} ]] && [[ {Oropt1} ]] || [[ {Oropt2} ]]; then
+# code if true
+else
+# code if false
+fi
+
+arraynames = ' name1 name2 name3 name4'
+for name in arraynames
+do
+echo arraynames
+done
+echo Finish
+
+
