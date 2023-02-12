30a74c90545108e8a52730d5897184f5cd29352b
Zeabreeze <ibenjaba@our.ecu.edu.au>
2023-02-12 13:29:40 +0000

    Assessment 4 prep

---
+++ A4/passwordCheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+reset="\e[0m"
+
+#Ask the user to type a secret password
+echo -e "${blue}"
+read -rsp "What is your secret password: " password
+
+#set the variables to compare
+val_a="./secret.txt"
+val_h=$(echo -n "$password" | sha256sum) 
+c_hash=$(cat "$val_a") 
+
+#Check the user's password against the hash stored
+if [ "$val_h" == "$c_hash" ]; then
+
+#if the user's password is correct or not, print relevant messages
+    echo -e "${green}\nAccess Granted"
+    # echo -e "Exit code 0"
+    exit 0
+else
+    echo -e "${red}\nAccess Denied"
+    echo -e "Exit code 1${reset}"
+    exit 1
+fi
