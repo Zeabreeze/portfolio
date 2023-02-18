a4b1a7629dfda98a1686ac736865d84e2a27df34
Zeabreeze <ibenjaba@our.ecu.edu.au>
2023-02-18 09:11:49 +0000

    A test only assessment 4 folder

---
+++ A4/testa2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#x=$(( 20 - 11 / 2 ))
+#echo -e $x
+
+#exit 0
+
+#greet_name ()
+
+#        echo "Hello $1"
+
+#greet_jessie ()
+
+#    echo "Hello Jessie"
+
+#echo "Hello everyone"
+#greet_jessie
+#read -p "please type your name"
+#greet_nam "$REPLY"
+#echo "Goodbye!"
+#exit 0
+
+number=20
+func()
+{
+  number=5
+}
+func
+echo -e "$(( number++ ))"
+
+#func()
+
+ #   number=5
+#  echo $(( number+number ))
+
+#x=$(( 3 + 21 / 4 ))
+#echo -e $x
+
+#echo -e "\033[31mERROR\033[0m"
+
+#exit 0
+
+#searchForItems()
+
+ #       grep "items" $1
+
+#shoppinglist.txt
+
+#searchForItems "shoppinglist.txt"
+
+#cat shoppinglist.txt | grep -E "[[:digit:]]{3}"
+
+#cat shoppinglist.txt | grep -E "^hours.*[aeiou]$"
+
+#./readlogs.sh | grep ERROR
+
+#awk '
+
+#   print $2
+#' input.txt
+
+#sed '8a\pluto' shoppinglist.txt
+
+#sed '/coffee/ d' shoppinglist.txt
