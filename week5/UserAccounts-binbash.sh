c7462dd1110e4c5a18a8219173b235542ef82171
Zeabreeze <ibenjaba@our.ecu.edu.au>
2023-02-19 15:21:37 +0000

    week 5 scripts

--- week5/UserAccounts-binbash.sh
+++ week5/UserAccounts-binbash.sh
@@ -12,19 +12,22 @@ cat /etc/passwd > data.txt
 # Using a field separator ":" to output values to the table
 # Allow colour formatting and spacing for a nice looking table
 # Use Regex to make the awk script filter the data to 
-# only show users who have “/bin/bash” listed as their default shell.
+# only show users who have “/bin/bash” listed as their default shell#.
+#print "______________________________________________________________________________________________\n";  
+#print "|    \033[34mUsername\033[0m     |  \033[34mUserID\033[0m   |  \033[34mGroupID\033[0m |               \033[34mHome\033[0m              |      \033[34mShell\033[0m";  
+#print "----------------------------------------------------------------------------------------------\n";  
 
 echo -e "User Accounts:"  
 awk 'BEGIN {  
     FS=":";  
-    print "______________________________________________________________________________________________\n";  
-    print "|    \033[34mUsername\033[0m     |  \033[34mUserID\033[0m   |  \033[34mGroupID\033[0m |               \033[34mHome\033[0m              |      \033[34mShell\033[0m";  
-    print "----------------------------------------------------------------------------------------------\n";  
-}  
+    print "___________________________________________________________________________________\n";  
+    printf("|\033[34m%-16s\033[0m|\033[34m%-11s\033[0m|\033[34m%-11s\033[0m|\033[34m%-30s\033[0m | \033[34m%-13s\033[0m\n", "Username", "UserID", "GroupID", "Home", "Shell");  
+    print "-----------------------------------------------------------------------------------\n";
+}
 
 {  
     if($NF == "/bin/bash")
-    printf("|\033[33m%-16s\033[0m | \033[35m%-9s\033[0m |\033[32m%-9s\033[0m |\033[31m%-32s\033[0m |\033[32m%-19s\033[0m\n", $1, $3, $4, $6, $7);  
+    printf("|\033[33m%-16s\033[0m | \033[35m%-9s\033[0m |\033[32m%-9s\033[0m |\033[31m%-30s\033[0m |\033[32m%-19s\033[0m\n", $1, $3, $4, $6, $7);  
 }  
 END {  
     print("______________________________________________________________________________________________");  
